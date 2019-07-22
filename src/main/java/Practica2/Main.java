93e0bc98d3217b2e1adefc93434ac98d3773db40
Edgar Núñez <EJNunez1311@users.noreply.github.com>
2019-07-22 15:09:48 -0400

    Demo

--- src/main/java/Practica2/Main.java
+++ src/main/java/Practica2/Main.java
@@ -3,7 +3,15 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
 
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
 
+        setPort(port);
         new Template().manejoTemplate();
     }
 }
